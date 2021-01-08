6a3cc40630489f9978619d49b0a0afd88ad8b4aa
kaancayli <y.k.cayli@gmail.com>
2021-01-09 00:37:35 +0300

    Done Exercises 3.7-3.8

--- part3/index.js
+++ part3/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
-
+const morgan = require('morgan');
 const app = express();
 
 app.use(express.json());
+app.use(morgan(':method :url :status :req[content-length] - :response-time ms :body'));
+
+morgan.token('body', function (req, res) { 
+        if(req.method === 'POST') {
+            return JSON.stringify(req.body);
+        }
+        return "";
+})
+  
 
 let persons = [
     {
