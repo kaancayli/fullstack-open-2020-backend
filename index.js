b6e57d00aca796ee9ba57c4130f9ce9deb4b94e3
kaancayli <y.k.cayli@gmail.com>
2021-01-10 01:47:04 +0300

    Created build

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 
 
 app.use(express.json());
+app.use(express.static('build'))
 app.use(cors());
 app.use(morgan(':method :url :status :req[content-length] - :response-time ms :body'));
 
