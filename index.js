d3c421eef12dd8f88d0984c3284df9a401e9eff5
kaancayli <y.k.cayli@gmail.com>
2021-01-28 00:35:27 +0100

    Bugfix HOST info added again

--- index.js
+++ index.js
@@ -97,8 +97,8 @@ app.get('/info', (request, response) => {
     `);
 })
 
-const HOST = 0.0.0.0;
+const HOST = '0.0.0.0';
 const PORT = process.env.PORT || 3001
-app.listen(PORT,HOST, () => {
+app.listen(PORT, HOST , () => {
   console.log(`Server running on port ${PORT}`)
 })
